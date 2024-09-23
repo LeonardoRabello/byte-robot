c50b1acc5ddc15959753018e7fd0ee6eeec88b4c
LeonardoRabello <16726190760@cefet-rj.br>
2024-09-23 20:52:09 -0300

    updating matrix code

--- raspberry/matrix.py
+++ raspberry/matrix.py
@@ -1,20 +1,50 @@
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
-from PIL import Image
-
+import time
+import json
+print(1)
+with open("../faces.json", 'r') as file:
+    faces = json.load(file)
+print(1)
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, width=16, height=8, block_orientation=90, rotate=0)
-
-def desenhar():
+device = max7219(serial, width=8, height=8, block_orientation=90, rotate=0)
+def draw_bitmap(bitmap):
     with canvas(device) as draw:
-        draw.point((1, 1), fill="white")
-        draw.point((6, 1), fill="white")
-        draw.point((3, 5), fill="white")
-        draw.point((4, 5), fill="white")
-        draw.point((5, 5), fill="white")
-        draw.arc((2, 2, 6, 6), start=45, end=135, fill="white")
+        for y in range(8):
+            for x in range(8):
+                if bitmap[y] & (1<<(15-x)):
+                    draw.point((x, y), fill='white')
+print(1)
+face_bitmap = [0b11100001,
+               0b00000001,
+               0b00000001,
+               0b00000000,
+               0b00000000,
+               0b10000000,
+               0b10000000,
+               0b10000111]
+print(1)
+try:
+    while True:
+        print(1)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        draw_bitmap(face_bitmap)
+        time.sleep(1)
+except KeyboardInterrupt:
+        pass
+    
 
-desenhar()
-        
-        
+
